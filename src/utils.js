9e06d946511c72486b70af15fe4a86ae438d9017
illi <illi@github.com>
2024-12-14 11:38:02 -0600

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,179 @@
 import { crawlerService } from './services/crawler-service';
+import puppeteer from 'puppeteer';
 
-export async function setupBrowser() {
+const isDev = process.env.NODE_ENV === 'development';
+const isRender = process.env.RENDER === '1' || process.env.RENDER === 'true';
+const isDebug = process.argv.includes('--debug');
+
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+
+async function setupBrowser() {
   try {
-    const browser = await crawlerService.getBrowser();
-    if (!browser) {
-      throw new Error('Browser initialization failed');
-    }
+    const options = {
+      headless: !isDev,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu',
+        '--window-size=1920,1080',
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins,site-per-process',
+        '--disable-site-isolation-trials',
+        '--disable-breakpad',
+        '--disable-sync',
+        '--no-first-run',
+        '--no-experiments',
+        '--no-default-browser-check',
+        '--disable-infobars',
+        '--disable-translate',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-renderer-backgrounding',
+        '--user-agent=' + USER_AGENT,
+        '--enable-javascript',
+        '--disable-notifications',
+        '--lang=en-US,en',
+        '--start-maximized'
+      ],
+      ignoreDefaultArgs: ['--enable-automation'],
+      defaultViewport: null
+    };
+
+    console.log('Launching browser with options:', {
+      headless: options.headless,
+      args: options.args
+    });
+
+    const browser = await puppeteer.launch(options);
     return browser;
   } catch (error) {
-    console.error('Failed to setup browser:', error);
+    console.error('Browser setup failed:', error);
     throw error;
   }
 }
 
-export async function setupPage(browser) {
-  if (!browser) {
-    throw new Error('Browser is required');
-  }
-  
-  try {
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.setExtraHTTPHeaders({
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'
+async function setupPage(browser) {
+  const page = await browser.newPage();
+  await page.setUserAgent(USER_AGENT);
+
+  // Set longer timeouts
+  await page.setDefaultTimeout(90000);
+  await page.setDefaultNavigationTimeout(90000);
+
+  // Set geolocation permissions
+  const context = page.browserContext();
+  await context.overridePermissions('https://www.stubhub.com', ['geolocation']);
+
+  // Enhanced stealth settings
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+    Object.defineProperty(navigator, 'plugins', {
+      get: () => [
+        {
+          0: {
+            type: "application/x-google-chrome-pdf",
+            suffixes: "pdf",
+            description: "Portable Document Format",
+            enabledPlugin: Plugin
+          },
+          description: "Portable Document Format",
+          filename: "internal-pdf-viewer",
+          length: 1,
+          name: "Chrome PDF Plugin"
+        }
+      ]
     });
-    return page;
-  } catch (error) {
-    console.error('Failed to setup page:', error);
-    throw error;
-  }
+    window.chrome = {
+      app: {
+        isInstalled: false,
+        InstallState: {
+          DISABLED: 'disabled',
+          INSTALLED: 'installed',
+          NOT_INSTALLED: 'not_installed'
+        },
+        RunningState: {
+          CANNOT_RUN: 'cannot_run',
+          READY_TO_RUN: 'ready_to_run',
+          RUNNING: 'running'
+        }
+      },
+      runtime: {
+        OnInstalledReason: {
+          CHROME_UPDATE: 'chrome_update',
+          INSTALL: 'install',
+          SHARED_MODULE_UPDATE: 'shared_module_update',
+          UPDATE: 'update'
+        },
+        OnRestartRequiredReason: {
+          APP_UPDATE: 'app_update',
+          OS_UPDATE: 'os_update',
+          PERIODIC: 'periodic'
+        },
+        PlatformArch: {
+          ARM: 'arm',
+          ARM64: 'arm64',
+          MIPS: 'mips',
+          MIPS64: 'mips64',
+          X86_32: 'x86-32',
+          X86_64: 'x86-64'
+        },
+        PlatformNaclArch: {
+          ARM: 'arm',
+          MIPS: 'mips',
+          MIPS64: 'mips64',
+          X86_32: 'x86-32',
+          X86_64: 'x86-64'
+        },
+        PlatformOs: {
+          ANDROID: 'android',
+          CROS: 'cros',
+          LINUX: 'linux',
+          MAC: 'mac',
+          OPENBSD: 'openbsd',
+          WIN: 'win'
+        },
+        RequestUpdateCheckStatus: {
+          NO_UPDATE: 'no_update',
+          THROTTLED: 'throttled',
+          UPDATE_AVAILABLE: 'update_available'
+        }
+      }
+    };
+    Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
+    Object.defineProperty(navigator, 'vendor', { get: () => 'Google Inc.' });
+
+    // Add WebGL support
+    const getParameter = WebGLRenderingContext.prototype.getParameter;
+    WebGLRenderingContext.prototype.getParameter = function(parameter) {
+      // Spoof renderer info
+      if (parameter === 37445) {
+        return 'Intel Inc.';
+      }
+      if (parameter === 37446) {
+        return 'Intel Iris OpenGL Engine';
+      }
+      return getParameter.apply(this, [parameter]);
+    };
+  });
+
+  // Set convincing headers
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1'
+  });
+
+  return page;
 }
 
-export function formatPrice(price) {
-  if (typeof price === 'string') {
-    return parseFloat(price.replace(/[^0-9.]/g, ''));
-  }
-  return price;
-}
+export { setupBrowser, setupPage, formatPrice };
